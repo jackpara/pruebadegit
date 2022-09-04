bb1b3f410e66a0e59d2967eeb97292a456ed3c32
jackpara <jackdavid3456@gmail.com>
2022-09-04 16:05:18 -0300

    subo el ejercicio 2 del primer pdf de python

---
+++ ejer1.2.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      User
+#
+# Created:     04/09/2022
+# Copyright:   (c) User 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+
+#EJER.2
+    cadena = "hola"
+    print(cadena +" mundo")
+
+if __name__ == '__main__':
+    main()
