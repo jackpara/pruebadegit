1036e1b1a72345a054d0c22569011a1d0cb49ac5
jackpara <jackdavid3456@gmail.com>
2022-09-04 16:06:56 -0300

    subo el ejercicio 4 del primer pdf de python

---
+++ ejer1.4.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module4
+# Purpose:
+#
+# Author:      User
+#
+# Created:     04/09/2022
+# Copyright:   (c) User 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+
+    '''
+    DEL EJER 3
+    EL CODIGO PIDE QUE SE LLENEN LOS CAMPOS CON LOS DATOS DEL USUARIO
+    USANDO ++nombre = input()++
+    Y LOS GUARDA EN UNA VARIABLE nombre.
+    PARA LUEGO MOSTRAR POR PANTALLA ESOS MISMOS DATOS
+    USANDO ++print(f'{nombre}'++)
+    COMO UN REGISTRO.
+    FIN.
+    '''
+
+if __name__ == '__main__':
+    main()
