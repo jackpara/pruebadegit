4a4f20052643dff0881d74233bcc8278660fc1dd
jackpara <jackdavid3456@gmail.com>
2022-09-04 16:06:12 -0300

    subo el ejercicio 3 del primer pdf de python

---
+++ ejer1.3.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      User
+#
+# Created:     04/09/2022
+# Copyright:   (c) User 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+
+#EJER.3
+    nombre = input("Ingrese su nombre")
+    dni = input("Ingrese su dni")
+
+    print(f'el NOMBRE del usuario es {nombre}'f' y su DNI es {dni}.')
+
+if __name__ == '__main__':
+    main()
